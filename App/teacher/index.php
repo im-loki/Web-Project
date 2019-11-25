5d14d1186b0dafc3a1d3e988af27fcfa94c39024
im-loki <37450759+im-loki@users.noreply.github.com>
2019-11-25 13:17:11 +0530

    Cleanup

--- App/teacher/index.php
+++ App/teacher/index.php
@@ -7,7 +7,7 @@
   if (isset($_GET['logout'])) {
   	session_destroy();
   	unset($_SESSION['username']);
-  	header("location: login.php");
+    header("location: login.php");
   }
 ?>
 <!DOCTYPE html>
